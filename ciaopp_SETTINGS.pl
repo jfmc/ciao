65b2b4dd6728ebd448a27ac6845a9bdc2ad3ddfe
Jose F. Morales <jfmcjf@gmail.com>
2025-06-20 22:51:47 +0000

    updated2

--- ciaopp_SETTINGS.pl
+++ ciaopp_SETTINGS.pl
@@ -31,35 +31,36 @@ doc_domains := part_domains-(~findall(X,doc_domain(X))).
 
 % All available domains
 % TODO: Synchronize automatically; Do "M-x ciao-grep-root" with ":- dom_def(" to find all definitons.
-doc_domain(aeq). % :- dom_def(aeq).
-doc_domain(def). % :- dom_def(def).
-doc_domain(deftypes). % :- dom_def(deftypes).
-doc_domain(depthk). % :- dom_def(depthk).
-doc_domain(eterms). % :- dom_def(eterms).
-doc_domain(etermsvar). % :- dom_def(etermsvar).
-doc_domain(frdef). % :- dom_def(frdef).
-doc_domain(fr_top). % :- dom_def(fr).
-doc_domain(gr). % :- dom_def(gr).
-doc_domain(lsign). % :- dom_def(lsign).
-doc_domain(lsigndiff). % :- dom_def(difflsign).
+%doc_domain(aeq). % :- dom_def(aeq).
+%doc_domain(def). % :- dom_def(def).
+%doc_domain(deftypes). % :- dom_def(deftypes).
+%doc_domain(depthk). % :- dom_def(depthk).
+%doc_domain(eterms). % :- dom_def(eterms).
+%doc_domain(etermsvar). % :- dom_def(etermsvar).
+%doc_domain(frdef). % :- dom_def(frdef).
+%doc_domain(fr_top). % :- dom_def(fr).
+%doc_domain(gr). % :- dom_def(gr).
+%doc_domain(lsign). % :- dom_def(lsign).
+%doc_domain(lsigndiff). % :- dom_def(difflsign).
+%%:- if(defined(bug)).
+%doc_domain(detplai). % :- dom_def(det).
+%doc_domain(nfplai). % :- dom_def(nf).
+%doc_domain(nonrel_intervals). % :- dom_def(nonrel_intervals).
+%:- if(defined(has_ciaopp_fpnum)).
+%doc_domain(nonrel_fintervals). % :- dom_def(nonrel_fintervals).
+%:- endif.
+%doc_domain(pd). % :- dom_def(pd).
+%doc_domain(pdb). % :- dom_def(pdb).
+%doc_domain(polyhedra). % :- dom_def(polyhedra).
+%doc_domain(ptypes). % :- dom_def(ptypes).
+%doc_domain(sharefree). % :- dom_def(shfr).
+%doc_domain(sharefree_amgu). % :- dom_def(sharefree_amgu).
+%doc_domain(sharefree_clique). % :- dom_def(sharefree_clique).
+%doc_domain(sharefree_clique_def). % :- dom_def(sharefree_clique_def).
+%doc_domain(sharefree_non_var). % :- dom_def(shfrnv).
+%doc_domain(shareson). % :- dom_def(shareson).
 %:- if(defined(bug)).
-doc_domain(detplai). % :- dom_def(det).
-doc_domain(nfplai). % :- dom_def(nf).
-doc_domain(nonrel_intervals). % :- dom_def(nonrel_intervals).
-:- if(defined(has_ciaopp_fpnum)).
-doc_domain(nonrel_fintervals). % :- dom_def(nonrel_fintervals).
-:- endif.
-doc_domain(pd). % :- dom_def(pd).
-doc_domain(pdb). % :- dom_def(pdb).
-doc_domain(polyhedra). % :- dom_def(polyhedra).
-doc_domain(ptypes). % :- dom_def(ptypes).
-doc_domain(sharefree). % :- dom_def(shfr).
-doc_domain(sharefree_amgu). % :- dom_def(sharefree_amgu).
-doc_domain(sharefree_clique). % :- dom_def(sharefree_clique).
-doc_domain(sharefree_clique_def). % :- dom_def(sharefree_clique_def).
-doc_domain(sharefree_non_var). % :- dom_def(shfrnv).
-doc_domain(shareson). % :- dom_def(shareson).
-%:- if(defined(bug)).
+% BUG HERE BEGIN
 doc_domain(sharing). % :- dom_def(share).
 doc_domain(sharing_amgu). % :- dom_def(share_amgu).
 doc_domain(sharing_clique). % :- dom_def(share_clique).
@@ -69,26 +70,27 @@ doc_domain(shfret). % :- dom_def(shfret).
 doc_domain(shfrlin_amgu). % :- dom_def(shfrlin_amgu).
 doc_domain(shfrson). % :- dom_def(shfrson).
 doc_domain(sondergaard). % :- dom_def(son).
-:- if(defined(bug)).
-doc_domain(svterms). % :- dom_def(svterms).
-doc_domain(termsd). % :- dom_def(terms).
-doc_domain(top_path_sharing). % :- dom_def(path).
-:- if(defined(has_ciaopp_cost)).
-doc_domain(sized_types). % :- dom_def(sized_types).
-doc_domain(res_plai_stprf). % :- dom_def(res_plai_stprf).
-doc_domain(res_plai). % :- dom_def(res_plai).
-:- endif.
-:- if(defined(has_ciaopp_java)).
-doc_domain(oo_types). % :- dom_def(oo_types).
-doc_domain(oo_son). % :- dom_def(oo_son).
-doc_domain(oo_shnltau). % :- dom_def(oo_shnltau).
-doc_domain(java_nullity). % :- dom_def(java_nullity).
-doc_domain(java_cha). % :- dom_def(java_cha).
-:- endif.
-%:- if(defined(has_ciaopp_bshare)).
-%doc_domain('bshare/bshare'). % :- dom_def(bshare).
+% BUG HERE STOP
+%:- if(defined(bug)).
+%doc_domain(svterms). % :- dom_def(svterms).
+%doc_domain(termsd). % :- dom_def(terms).
+%doc_domain(top_path_sharing). % :- dom_def(path).
+%:- if(defined(has_ciaopp_cost)).
+%doc_domain(sized_types). % :- dom_def(sized_types).
+%doc_domain(res_plai_stprf). % :- dom_def(res_plai_stprf).
+%doc_domain(res_plai). % :- dom_def(res_plai).
 %:- endif.
-:- endif.
+%:- if(defined(has_ciaopp_java)).
+%doc_domain(oo_types). % :- dom_def(oo_types).
+%doc_domain(oo_son). % :- dom_def(oo_son).
+%doc_domain(oo_shnltau). % :- dom_def(oo_shnltau).
+%doc_domain(java_nullity). % :- dom_def(java_nullity).
+%doc_domain(java_cha). % :- dom_def(java_cha).
+%:- endif.
+%%:- if(defined(has_ciaopp_bshare)).
+%%doc_domain('bshare/bshare'). % :- dom_def(bshare).
+%%:- endif.
+%:- endif.
 
 doc_other_commands :=
     part_other_commands-[
